33df16c82a621c61f75e287aa7a4b9530028c7dc
Renato <re.fbarcellos@hotmail.com>
2020-07-16 08:14:15 -0400

    mask generator to compleet images autonatic

--- src/panGenerator/paint.py
+++ src/panGenerator/paint.py
@@ -1,31 +1,47 @@
 import numpy as np
 import cv2
 
-
-#FIXME: caso resolva usar esse metodo
-# Fazer uma mascara gerada automaticamente
-
-def paint():
-    try: #carregando as imagens
-        img = cv2.imread('Panoramica.png')
-        
-        mask = cv2.imread('./exe1/mask.png',0)
-
+def paint(image):
+    try:
+        image = cv2.imread(image)
+        mask = cv2.imread('mask.png',0)
     except cv2.error as e:
-        print('Invalid frame!\n\n', e)
+        print(e)
     cv2.waitKey()
 
-    painted = cv2.inpaint(img,mask,3,cv2.INPAINT_TELEA)
+
+    painted = cv2.inpaint(image,mask,3,cv2.INPAINT_TELEA)
 
     try: #retornando a imagem
         cv2.imshow('pintada', painted) #mostrando a imagem
         cv2.imwrite('painted.png', painted)
         cv2.waitKey() #assim que alguma tecla for pressionada as imagem se fecha
-        #cv2.imwrite(namePan, espelhada) #salvando imagem na mesma pasta do script
         
     except cv2.error as e:
         print('Algo deu errado, tente novamente\n\n',e)
         cv2.waitKey()
 
+def mkMask(image):
 
-paint()
+    try:
+        img = cv2.imread(image)
+    except cv2.error as e:
+        print(e)
+
+    myColor = [ 0,0,0,0,0,0]
+
+    try:
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower = np.array(myColor[0:3])
+        upper = np.array(myColor[3:6])
+        mask = cv2.inRange(imgHSV,lower,upper)
+        cv2.imwrite("mask.png",mask)
+    except cv2.error as e:
+        print(e)
+
+panoramica = 'cilynder.jpg'
+
+mkMask(panoramica)
+paint(panoramica)
+
+
