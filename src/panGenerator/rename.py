dfb66504a65cabdbd4ce24e94f54fa233712a977
Renato <re.fbarcellos@hotmail.com>
2020-07-21 20:48:36 -0400

    some improves

--- src/panGenerator/rename.py
+++ src/panGenerator/rename.py
@@ -1,10 +1,10 @@
 # Este script renomeia as imagens de input para serem imagens com nomes de 0 a x
 import os
 from glob import glob
-
-for number, filename in enumerate(glob("./input/*.png")):
+number = 15
+for number , filename in enumerate(glob("./input/*.jpg")):
     try:
-        os.rename(filename, "./input/{0}.png".format(number))
+        os.rename(filename, "./input/{0}.jpg".format(number))
     except OSError as e:
         print("Something happened:", e)
 
